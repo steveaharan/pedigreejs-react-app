fda96bbd0302ecbc8b548cbceb3f0bacf61d50e3
tcarver <tjc29@medschl.cam.ac.uk>
2024-12-20 12:12:22 +0000

    tidy

--- src/PedigreeJS.jsx
+++ src/PedigreeJS.jsx
@@ -34,11 +34,10 @@ export function PedigreeJS() {
 					{'type': 'prostate_cancer', 'colour': '#D5494A'}],
       'DEBUG': false};
 
-	  
-	  var local_dataset = pedigreejs_pedcache.current(opts);
-	  if (local_dataset !== undefined && local_dataset !== null) {
+	let local_dataset = pedigreejs_pedcache.current(opts);
+	if (local_dataset !== undefined && local_dataset !== null) {
 		opts.dataset = local_dataset;
-	  } else {
+	} else {
 		opts.dataset = [
 			{"name": "m11", "sex": "M", "top_level": true},
 			{"name": "f11", "display_name": "Jane",  "sex": "F", "status": 1, "top_level": true, "breast_cancer_diagnosis_age":67, "ovarian_cancer_diagnosis_age":63},
@@ -48,10 +47,10 @@ export function PedigreeJS() {
 			{"name": "f21", "sex": "F", "mother": "f12", "father": "m12", "breast_cancer_diagnosis_age":55, "breast_cancer2_diagnosis_age": 60, "ovarian_cancer_diagnosis_age":58, "age": 63},
 			{"name": "ch1", "display_name": "Ana", "sex": "F", "mother": "f21", "father": "m21", "proband": true, "age": 25, "yob": 1996}
 		];
-	  }
-		
-	  let resizeTimer;
-	  function handleResize() {
+	}
+
+	let resizeTimer;
+	function handleResize() {
         clearTimeout(resizeTimer);
         if(dimensions.width === window.innerWidth && dimensions.height === window.innerHeight) {
           return;
@@ -68,8 +67,8 @@ export function PedigreeJS() {
         }, 500);
 	 }
 
-    // listen and handle browser window resizing
-    useEffect(() => {
+	// listen and handle browser window resizing
+	useEffect(() => {
       const firstRender = ref.current;
       if (firstRender) {
         ref.current = false;
@@ -78,9 +77,9 @@ export function PedigreeJS() {
 
 	  // if medium size screen or above [removing this fixes page returning to top on mobile devices when scrolling]
 	  if(dimensions.width >= 768) window.addEventListener('resize', handleResize);
-    });
-
-    return (
+	});
+	
+	return (
 		<>
 			<div id="pedigree_history" className="p-2"></div>
 			<div key="tree" id="pedigree"></div>
@@ -88,43 +87,40 @@ export function PedigreeJS() {
     )
 }
 
-
 function convertRemToPixels(rem) {    
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
 /** Show pedigreejs **/
 const showPedigree = (opts) => {
-  const p = document.getElementById("pedigreejs");
-  if(p) {
-    opts["width"] = p.offsetWidth - convertRemToPixels(8);
-	console.log(opts.width)
-  }
-  const ped = document.getElementById("pedigree");
-  if(!p && ped){
-    console.log("REDRAW", opts.width);
-    const p = document.createElement('div');
-    p.id = 'pedigreejs';
-    ped.appendChild(p); 
-    pedigreejs_load(opts);
-  }
-  const fscreen = document.getElementById("fullscreen");
-  fscreen.style.display = "none";
-  const refresh = document.getElementsByClassName("fa-refresh");
-  if(refresh) refresh[0].style.display = "none";
+	const p = document.getElementById("pedigreejs");
+	if(p) {
+		opts["width"] = p.offsetWidth - convertRemToPixels(8);
+	}
+	const ped = document.getElementById("pedigree");
+	if(!p && ped){
+		const p = document.createElement('div');
+		p.id = 'pedigreejs';
+		ped.appendChild(p); 
+		pedigreejs_load(opts);
+	}
+	const fscreen = document.getElementById("fullscreen");
+	fscreen.style.display = "none";
+	const refresh = document.getElementsByClassName("fa-refresh");
+	if(refresh) refresh[0].style.display = "none";
 }
 
 const pedigreejs_load = (opts) => {
-  try {
-    pedigreejs.rebuild(opts);
-    pedigreejs_zooming.scale_to_fit(opts);
-  } catch(e) {
-    let msg;
-    if (typeof e === "string") {
-        msg = e.toUpperCase();
-    } else if (e instanceof Error) {
-        msg = e.message;
-    }
-    console.error("FamilyTree ::: "+msg, e);
-  }
+	try {
+		pedigreejs.rebuild(opts);
+		pedigreejs_zooming.scale_to_fit(opts);
+	} catch(e) {
+		let msg;
+		if (typeof e === "string") {
+			msg = e.toUpperCase();
+		} else if (e instanceof Error) {
+			msg = e.message;
+		}
+		console.error("FamilyTree ::: "+msg, e);
+	}
 };
