e1217dc0972a5ae922c3f0594794ddba6f09b1c9
Steve <104828517+steveaharan@users.noreply.github.com>
2025-07-14 08:48:40 +0100

    finally

--- src/PedigreeJS.jsx
+++ src/PedigreeJS.jsx
@@ -114,48 +114,54 @@ class Person {
 	}
 }
 
-// Family data using Person class
+// [
+//   {
+//     "famid": "XFAM",
+//     "display_name": "0",
+//     "name": "parent3",
+//     "sex": "F",
+//     "status": "0",
+//     "father": null,
+//     "mother": null,
+//     "age": "55",
+//     "breast_cancer_diagnosis_age": "53",
+//     "level": 2,
+//     "noparents": true
+//   },
+//   {
+//     "famid": "XFAM",
+//     "display_name": "0",
+//     "name": "parent4",
+//     "sex": "M",
+//     "status": "0",
+//     "age": "60",
+//     "level": 2,
+//     "noparents": true,
+//     "mother": null,
+//     "father": null
+//   },
+//   {
+//     "famid": "XFAM",
+//     "display_name": "1",
+//     "name": "child7",
+//     "sex": "F",
+//     "status": "0",
+//     "father": "parent4",
+//     "mother": "parent3",
+//     "age": "25",
+//     "yob": "2000"
+//   }
+// ]
+
+
 const createFamilyData = () => {
 	return [
-		new Person({
-			id: 'grandma',
-			name: 'grandma',
-			sex: 'F',
-			age: 85,
-			yearOfBirth: 1933,
-			isDead: true,
-			diseases: { breast_cancer: 55 }
-		}),
-		new Person({
-			id: 'grandpa',
-			name: 'grandpa',
-			sex: 'M',
-			age: 88,
-			yearOfBirth: 1930,
-			isDead: true,
-			diseases: { prostate_cancer: 71 }
-		}),
-		new Person({
-			id: 'parent1',
-			name: 'parent1',
-			sex: 'F',
-			age: 50
-		}),
-		new Person({
-			id: 'parent2',
-			name: 'parent2',
-			sex: 'M',
-			age: 60,
-			fatherId: 'grandpa',
-			motherId: 'grandma'
-		}),
+	
 		new Person({
 			id: 'parent3',
 			name: 'parent3',
 			sex: 'F',
 			age: 55,
-			fatherId: 'grandpa',
-			motherId: 'grandma',
 			diseases: { breast_cancer: 53 }
 		}),
 		new Person({
@@ -163,55 +169,6 @@ const createFamilyData = () => {
 			name: 'parent4',
 			sex: 'M',
 			age: 60
-		}),
-		new Person({
-			id: 'child1',
-			name: 'child1',
-			sex: 'F',
-			age: 40,
-			fatherId: 'parent2',
-			motherId: 'parent1',
-			diseases: { breast_cancer: 40 }
-		}),
-		new Person({
-			id: 'child2',
-			name: 'child2',
-			sex: 'F',
-			age: 38,
-			fatherId: 'parent2',
-			motherId: 'parent1'
-		}),
-		new Person({
-			id: 'child3',
-			name: 'child3',
-			sex: 'F',
-			age: 36,
-			fatherId: 'parent2',
-			motherId: 'parent1'
-		}),
-		new Person({
-			id: 'child4',
-			name: 'child4',
-			sex: 'M',
-			age: 36,
-			fatherId: 'parent2',
-			motherId: 'parent1'
-		}),
-		new Person({
-			id: 'child5',
-			name: 'child5',
-			sex: 'F',
-			age: 36,
-			fatherId: 'parent2',
-			motherId: 'parent1'
-		}),
-		new Person({
-			id: 'child6',
-			name: 'child6',
-			sex: 'M',
-			age: 36,
-			fatherId: 'parent2',
-			motherId: 'parent1'
 		}),
 		new Person({
 			id: 'child7',
@@ -223,72 +180,184 @@ const createFamilyData = () => {
 			motherId: 'parent3',
 			isTarget: true
 		}),
-		new Person({
-			id: 'child8',
-			name: 'child8',
-			sex: 'F',
-			age: 38,
-			fatherId: 'parent4',
-			motherId: 'parent3'
-		}),
-		new Person({
-			id: 'child9',
-			name: 'child9',
-			sex: 'F',
-			age: 23,
-			fatherId: 'parent4',
-			motherId: 'parent3'
-		}),
-		new Person({
-			id: 'child10',
-			name: 'child10',
-			sex: 'M',
-			age: 35,
-			fatherId: 'parent2',
-			motherId: 'parent1'
-		}),
-		new Person({
-			id: 'child11',
-			name: 'child11',
-			sex: 'F',
-			age: 28,
-			fatherId: 'parent4',
-			motherId: 'parent3'
-		}),
-		new Person({
-			id: 'child12',
-			name: 'child12',
-			sex: 'M',
-			age: 0,
-			fatherId: 'child10',
-			motherId: 'child11'
-		}),
-		new Person({
-			id: 'child13',
-			name: 'child13',
-			sex: 'F',
-			age: 0,
-			fatherId: 'child10',
-			motherId: 'child11'
-		}),
-		new Person({
-			id: 'child14',
-			name: 'child14',
-			sex: 'M',
-			age: 0,
-			fatherId: 'child10',
-			motherId: 'child11'
-		}),
-		new Person({
-			id: 'child15',
-			name: 'child15',
-			sex: 'F',
-			age: 0,
-			fatherId: 'child10',
-			motherId: 'child11'
-		})
 	];
 };
+
+// Family data using Person class
+// const createFamilyData = () => {
+// 	return [
+// 		new Person({
+// 			id: 'grandma',
+// 			name: 'grandma',
+// 			sex: 'F',
+// 			age: 85,
+// 			yearOfBirth: 1933,
+// 			isDead: true,
+// 			diseases: { breast_cancer: 55 }
+// 		}),
+// 		new Person({
+// 			id: 'grandpa',
+// 			name: 'grandpa',
+// 			sex: 'M',
+// 			age: 88,
+// 			yearOfBirth: 1930,
+// 			isDead: true,
+// 			diseases: { prostate_cancer: 71 }
+// 		}),
+// 		new Person({
+// 			id: 'parent1',
+// 			name: 'parent1',
+// 			sex: 'F',
+// 			age: 50
+// 		}),
+// 		new Person({
+// 			id: 'parent2',
+// 			name: 'parent2',
+// 			sex: 'M',
+// 			age: 60,
+// 			fatherId: 'grandpa',
+// 			motherId: 'grandma'
+// 		}),
+// 		new Person({
+// 			id: 'parent3',
+// 			name: 'parent3',
+// 			sex: 'F',
+// 			age: 55,
+// 			fatherId: 'grandpa',
+// 			motherId: 'grandma',
+// 			diseases: { breast_cancer: 53 }
+// 		}),
+// 		new Person({
+// 			id: 'parent4',
+// 			name: 'parent4',
+// 			sex: 'M',
+// 			age: 60
+// 		}),
+// 		new Person({
+// 			id: 'child1',
+// 			name: 'child1',
+// 			sex: 'F',
+// 			age: 40,
+// 			fatherId: 'parent2',
+// 			motherId: 'parent1',
+// 			diseases: { breast_cancer: 40 }
+// 		}),
+// 		new Person({
+// 			id: 'child2',
+// 			name: 'child2',
+// 			sex: 'F',
+// 			age: 38,
+// 			fatherId: 'parent2',
+// 			motherId: 'parent1'
+// 		}),
+// 		new Person({
+// 			id: 'child3',
+// 			name: 'child3',
+// 			sex: 'F',
+// 			age: 36,
+// 			fatherId: 'parent2',
+// 			motherId: 'parent1'
+// 		}),
+// 		new Person({
+// 			id: 'child4',
+// 			name: 'child4',
+// 			sex: 'M',
+// 			age: 36,
+// 			fatherId: 'parent2',
+// 			motherId: 'parent1'
+// 		}),
+// 		new Person({
+// 			id: 'child5',
+// 			name: 'child5',
+// 			sex: 'F',
+// 			age: 36,
+// 			fatherId: 'parent2',
+// 			motherId: 'parent1'
+// 		}),
+// 		new Person({
+// 			id: 'child6',
+// 			name: 'child6',
+// 			sex: 'M',
+// 			age: 36,
+// 			fatherId: 'parent2',
+// 			motherId: 'parent1'
+// 		}),
+// 		new Person({
+// 			id: 'child7',
+// 			name: 'child7',
+// 			sex: 'F',
+// 			age: 25,
+// 			yearOfBirth: 2000,
+// 			fatherId: 'parent4',
+// 			motherId: 'parent3',
+// 			isTarget: true
+// 		}),
+// 		new Person({
+// 			id: 'child8',
+// 			name: 'child8',
+// 			sex: 'F',
+// 			age: 38,
+// 			fatherId: 'parent4',
+// 			motherId: 'parent3'
+// 		}),
+// 		new Person({
+// 			id: 'child9',
+// 			name: 'child9',
+// 			sex: 'F',
+// 			age: 23,
+// 			fatherId: 'parent4',
+// 			motherId: 'parent3'
+// 		}),
+// 		new Person({
+// 			id: 'child10',
+// 			name: 'child10',
+// 			sex: 'M',
+// 			age: 35,
+// 			fatherId: 'parent2',
+// 			motherId: 'parent1'
+// 		}),
+// 		new Person({
+// 			id: 'child11',
+// 			name: 'child11',
+// 			sex: 'F',
+// 			age: 28,
+// 			fatherId: 'parent4',
+// 			motherId: 'parent3'
+// 		}),
+// 		new Person({
+// 			id: 'child12',
+// 			name: 'child12',
+// 			sex: 'M',
+// 			age: 0,
+// 			fatherId: 'child10',
+// 			motherId: 'child11'
+// 		}),
+// 		new Person({
+// 			id: 'child13',
+// 			name: 'child13',
+// 			sex: 'F',
+// 			age: 0,
+// 			fatherId: 'child10',
+// 			motherId: 'child11'
+// 		}),
+// 		new Person({
+// 			id: 'child14',
+// 			name: 'child14',
+// 			sex: 'M',
+// 			age: 0,
+// 			fatherId: 'child10',
+// 			motherId: 'child11'
+// 		}),
+// 		new Person({
+// 			id: 'child15',
+// 			name: 'child15',
+// 			sex: 'F',
+// 			age: 0,
+// 			fatherId: 'child10',
+// 			motherId: 'child11'
+// 		})
+// 	];
+// };
 
 // CanRisk format handler class
 class CanRiskFormatter {
